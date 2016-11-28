6e0fa13985e81dcc5048c566698ebd18dd1a6c74
Balakrishnan (Balki) Ranganathan <balki@live.com>
2016-11-29 00:31:53 +0100

    Prepping for moving existing infra to this project

---
+++ Aide/Aide.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using AutoIt;
+
+namespace Aide.Desktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var handle = AutoItX.WinGetHandle("Lineage II");
+            AutoItX.ControlSend("Lineage II", "", "", "7");
+        }
+    }
+}
